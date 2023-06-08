62dbc27e2348bfe0ce52f0dbefb61f3451a0bd7d
kai3.ck3 <kai3.ck3@alibaba-inc.com>
2023-06-08 16:21:02 +0800

    add file

---
+++ chk_input_syy.sh
@@ -0,0 +1,63 @@
+echo "[$0] started, process id: $$, time is `date`"
+
+handleInput() {
+  echo "starting to input, now is `date +%Y-%m-%d\ %H:%M:%S\ 星期%w`"
+  echo "waking up the phone..."
+  input keyevent 82
+  sleep 1
+  echo "unlocking the phone..."
+  input swipe 500 1500 500 500 200
+  sleep 2
+  echo "killing the app..."
+  am force-stop com.alibaba.android.rimet
+  sleep 2
+  echo "restarting the app..."
+  am start -n com.alibaba.android.rimet/.biz.LaunchHomeActivity
+  sleep 10
+  echo "tapping step 1..."
+  input tap 540 2300
+  sleep 5
+  echo "tapping step 2..."
+  input tap 80 1000
+  sleep 5
+  echo "tapping step 3..."
+  input tap 540 1400
+  echo "input all done!!!!!!!"
+}
+
+while [ "a" != "b" ]
+do
+  nowWeek=`date +%w`
+  nowHour=`date +%H`
+  nowMinute=`date +%M`
+  echo "now is 星期$nowWeek $nowHour:$nowMinute"
+
+  if [ $nowWeek -le 5 ] && [ $nowWeek -gt 0 ]; then
+    if [ $nowHour -eq 8 ] && [ $nowMinute -ge 20 ] && [ $nowMinute -lt 30 ]; then
+      waitTime=$((RANDOM % 300))
+      echo "work day morning time is right, wait $waitTime seconds then do input."
+      sleep $waitTime
+      handleInput
+    elif [ $nowHour -eq 22 ] && [ $nowMinute -ge 10 ] && [ $nowMinute -lt 20 ]; then
+      waitTime=$((RANDOM % 300))
+      echo "work day night time is right, wait $waitTime seconds then do input."
+      sleep $waitTime
+      handleInput
+    fi
+  else
+    if [ $nowHour -eq 11 ] && [ $nowMinute -ge 20 ] && [ $nowMinute -lt 30 ]; then
+      waitTime=$((RANDOM % 300))
+      echo "weekend morning time is right, wait $waitTime seconds then do input."
+      sleep $waitTime
+      handleInput
+    elif [ $nowHour -eq 17 ] && [ $nowMinute -ge 10 ] && [ $nowMinute -lt 20 ]; then
+      waitTime=$((RANDOM % 300))
+      echo "weekend night time is right, wait $waitTime seconds then do input."
+      sleep $waitTime
+      handleInput
+    fi
+  fi
+
+  echo "waiting for next check..."
+  sleep 480s
+done
